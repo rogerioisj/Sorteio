dfc0938488482a67aac77f4c62cc3f2cf0f6f4c8
rogerioisj <rogerio.reaper@gmail.com>
2020-06-24 11:57:41 -0300

    App pronto e funcional

---
+++ app/src/main/java/com/rogeioisj/sorteio/MainActivity.java
@@ -0,0 +1,24 @@
+package com.rogeioisj.sorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNumero(View view){
+        TextView texto = findViewById(R.id.texto_resultado);
+        int numeroApresentado = new Random().nextInt(11);
+        texto.setText("-" + numeroApresentado + "-");
+    }
+}
